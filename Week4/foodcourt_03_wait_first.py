157a0077237dcafcc7dd02dd17694718f6b9ce70
nongjoun02@ <6720301002@cdti.ac.th>
2026-07-15 10:20:18 +0700

    Feat: update feature03

--- Week4/foodcourt_03_wait_first.py
+++ Week4/foodcourt_03_wait_first.py
@@ -1,27 +1,32 @@
 # foodcourt_03_wait_first.py
 import asyncio
 from time import ctime, time
-from turtle import done
 from food_utils import send_order_to_kitchen
 
-async def main(): 
-    my_student_id = "6720301002"
-    print(f"{ctime()} | --- [Task 3] Practice using wait (FirstCompleted) ---")
-    start_time = ctime()
+async def main():
+    MY_STUDENT_ID = "6720301001"
+    print(f"{ctime()} | --- [Task 3] Practice using wait (FIRST_COMPLETED) ---")
+    start_time = time()
 
-    #1 spawn 3 concurrent tasks to order food from different kitchens
-    t1 = asyncio.create_task(send_order_to_kitchen(my_student_id, "steak", "Sizzling Steak"))
-    t2 = asyncio.create_task(send_order_to_kitchen(my_student_id, "noodle", "Wonton Noodle"))
-    t3 = asyncio.create_task(send_order_to_kitchen(my_student_id, "hainanese_chicken", "Chicken Rice Thigh"))
+    # 1. Send 3 orders to compete against each other.
+    orders = {
+        asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "steak", "Beef Steak")),
+        asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "noodle", "Clear Soup Noodles")),
+        asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "hainanese_chicken", "Chicken Rice Thigh"))
+    }
 
-    done, pending = await asyncio.wait([t1, t2, t3], return_when=asyncio.FIRST_COMPLETED)
-    
-    fastest_task = list(done)[0].result()
-    print(f"{ctime()} | Winner served dish: Shop: {fastest_task['shop']} | Menu: {fastest_task['menu']}")
+    # 2. Use asyncio.wait with FIRST_COMPLETED to unblock as soon as any dish finishes.
+    done, pending = await asyncio.wait(orders, return_when=asyncio.FIRST_COMPLETED)
 
+    # 3. Get the result of the fastest dish that completed first.
+    fastest_dish = list(done)[0].result()
+    print(f"{ctime()} | Winner served dish: Shop: {fastest_dish['shop']} | Menu: {fastest_dish['menu']}")
+
+    # 4. Cancel the remaining pending tasks to save network resources.
     print(f"{ctime()} | Cleaning up: Canceling {len(pending)} remaining pending orders...")
     for t in pending:
         t.cancel()
+
     print(f"{ctime()} | Total waiting time for the first dish: {time() - start_time:.2f} seconds.")
 
 if __name__ == "__main__":
