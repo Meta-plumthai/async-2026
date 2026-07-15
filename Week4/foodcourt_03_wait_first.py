9c1c51388a99ff3439bfec4f2436a65b7f7b1837
nongjoun02@ <6720301002@cdti.ac.th>
2026-07-15 10:18:05 +0700

    Feat: add all feature in week4 foodcourt

--- Week4/foodcourt_03_wait_first.py
+++ Week4/foodcourt_03_wait_first.py
@@ -1,1 +1,28 @@
 # foodcourt_03_wait_first.py
+import asyncio
+from time import ctime, time
+from turtle import done
+from food_utils import send_order_to_kitchen
+
+async def main(): 
+    my_student_id = "6720301002"
+    print(f"{ctime()} | --- [Task 3] Practice using wait (FirstCompleted) ---")
+    start_time = ctime()
+
+    #1 spawn 3 concurrent tasks to order food from different kitchens
+    t1 = asyncio.create_task(send_order_to_kitchen(my_student_id, "steak", "Sizzling Steak"))
+    t2 = asyncio.create_task(send_order_to_kitchen(my_student_id, "noodle", "Wonton Noodle"))
+    t3 = asyncio.create_task(send_order_to_kitchen(my_student_id, "hainanese_chicken", "Chicken Rice Thigh"))
+
+    done, pending = await asyncio.wait([t1, t2, t3], return_when=asyncio.FIRST_COMPLETED)
+    
+    fastest_task = list(done)[0].result()
+    print(f"{ctime()} | Winner served dish: Shop: {fastest_task['shop']} | Menu: {fastest_task['menu']}")
+
+    print(f"{ctime()} | Cleaning up: Canceling {len(pending)} remaining pending orders...")
+    for t in pending:
+        t.cancel()
+    print(f"{ctime()} | Total waiting time for the first dish: {time() - start_time:.2f} seconds.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- Week4/foodcourt_05_mix_concepts.py
+++ Week4/foodcourt_05_mix_concepts.py
@@ -1,1 +1,25 @@
 # foodcourt_05_mix_concepts.py
+import asyncio
+from time import ctime, time
+from food_utils import send_order_to_kitchen
+
+async def main():
+    my_student_id = "6720301002"
+    print(f"{ctime()} | --- [Task 5] Advanced Practice: Mixing concepts together ---")
+    start_time = time()
+
+    noodle_task = asyncio.create_task(send_order_to_kitchen(my_student_id, "noodle", "Egg Noodle"))
+    chicken_task = asyncio.create_task(asyncio.wait_for(
+        send_order_to_kitchen(my_student_id, "hainanese_chicken", "Chicken Rice Special"), 
+        timeout=1.0
+    ))
+    try:
+        result = await asyncio.gather(noodle_task, chicken_task)
+        print(f"{ctime()} | Success: All food served on time! Received {len(result)} dishes.")
+    except asyncio.TimeoutError:
+        print(f"{ctime()} | Error: An unexpected timeout occurred during order processing.")
+
+    print(f"{ctime()} | Total elapsedtime: {time() - start_time:.2f} seconds.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
